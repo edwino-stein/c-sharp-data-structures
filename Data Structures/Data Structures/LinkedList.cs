71674101472a9918e276d11c76c09f7f08884cd2
Edwino Stein <edwino.stein@gmail.com>
2021-04-16 15:19:48 -0400

    Add LinkedList data structure

---
+++ Data Structures/Data Structures/LinkedList.cs
@@ -0,0 +1,212 @@
+﻿namespace DataStructures
+{
+    public class LinkedList<T>
+    {
+        private class Node {
+            public T value { get; set; }
+            public Node? next { get; set; }
+            public Node? prev { get; set; }
+
+            public Node(T value, Node next, Node prev)
+            {
+                this.value = value;
+                this.next = next;
+                this.prev = prev;
+            }
+
+            public Node(T value)
+            {
+                this.value = value;
+                this.next = null;
+                this.prev = null;
+            }
+
+            public bool HasNext()
+            {
+                return this.next != null;
+            }
+
+            public bool HasPrev()
+            {
+                return this.prev != null;
+            }
+        }
+
+        private Node? first;
+        private Node? last;
+        
+        public uint size { get; private set; }
+        
+        public LinkedList()
+        {
+            this.first = null;
+            this.last = null;
+        }
+
+        public bool Empty()
+        {
+            return this.size == 0;
+        }
+
+        private Node? findNode(uint index)
+        {
+            if (index >= this.size) return null;
+
+            Node node = this.first;
+            for (uint i = 0; i < index; ++i)
+            {
+                if (!node.HasNext()) return null;
+                node = node.next;
+            }
+
+            return node;
+        }
+
+        public T First()
+        {
+            return this.first != null ? this.first.value : default;
+        }
+
+        public T Last()
+        {
+            return this.last != null ? this.last.value : default;
+        }
+
+        public T At(uint index)
+        {
+            Node node = this.findNode(index);
+            return node != null ? node.value : default;
+        }
+
+        public bool PushBack(T value)
+        {
+            if (this.Empty())
+            {
+                this.first = new Node(value, null, null);
+                this.last = this.first;
+            }
+            else
+            {
+                this.last = new Node(value, null, this.last);
+                this.last.prev.next = this.last;
+            }
+
+            this.size += 1;
+            return true;
+        }
+
+        public bool PushFront(T value)
+        {
+            if (this.Empty())
+            {
+                this.first = new Node(value, null, null);
+                this.last = this.first;
+            }
+            else
+            {
+                this.first = new Node(value, this.first, null);
+                this.first.next.prev = this.first;
+            }
+
+            this.size += 1;
+            return true;
+        }
+
+        public bool Insert(T value, uint index)
+        {
+            if (index > this.size) return false;
+            if (index == 0) return this.PushFront(value);
+            if (index == this.size) return this.PushBack(value);
+
+            Node node = this.findNode(index);
+            if (node == null) return false;
+
+            Node newNode = new Node(value, node, node.prev);
+            node.prev.next = newNode;
+            node.prev = newNode;
+            this.size += 1;
+
+            return true;
+        }
+
+        public bool PopFront()
+        {
+            if (this.Empty()) return false;
+
+            if (this.first.HasNext())
+            {
+                this.first = this.first.next;
+                this.first.prev = null;
+            }
+            else
+            {
+                this.first = null;
+                this.last = null;
+            }
+
+            this.size -= 1;
+            return true;
+        }
+
+        public bool PopBack()
+        {
+            if (this.Empty()) return false;
+            if (this.last.HasPrev())
+            {
+                this.last = this.last.prev;
+                this.last.next = null;
+            }
+            else
+            {
+                this.first = null;
+                this.last = null;
+            }
+
+            this.size -= 1;
+            return true;
+        }
+
+        public bool Remove(uint index)
+        {
+            if (index >= this.size) return false;
+            if (index == 0) return this.PopFront();
+            if (index == this.size - 1) return this.PopBack();
+
+            Node node = this.findNode(index);
+            if (node == null) return false;
+
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+
+            this.size -= 1;
+            return true;
+        }
+
+        public delegate void EachHandle(T value, uint index);
+
+        public void Each(EachHandle handle, bool reverse = false)
+        {
+            if (this.Empty()) return;
+
+            if (!reverse)
+            {
+                Node node = this.first;
+                uint i = 0;
+                while (node != null)
+                {
+                    handle(node.value, i++);
+                    node = node.next;
+                }
+            }
+            else {
+                Node node = this.last;
+                uint i = this.size - 1;
+                while (node != null)
+                {
+                    handle(node.value, i--);
+                    node = node.prev;
+                }
+            }   
+        }
+    }
+}
---
+++ Data Structures/Test/LinkedListTest.cs
@@ -0,0 +1,236 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures;
+
+namespace DataStructuresTest
+{
+    [TestClass]
+    public class LinkedListTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            Assert.AreEqual(list.size, (uint) 0);
+            Assert.IsTrue(list.Empty());
+            Assert.AreEqual(list.First(), default);
+            Assert.AreEqual(list.Last(), default);
+            Assert.AreEqual(list.At(0), default);
+        }
+
+        [TestMethod]
+        public void PushBackTest()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            Assert.IsTrue(list.PushBack(1));
+            Assert.AreEqual(list.size, (uint) 1);
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 1);
+
+            Assert.IsTrue(list.PushBack(2));
+            Assert.AreEqual(list.size, (uint) 2);
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 2);
+
+            Assert.IsTrue(list.PushBack(3));
+            Assert.AreEqual(list.size, (uint) 3);
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 3);
+
+            for (uint i = 0; i < list.size; ++i)
+            {
+                Assert.AreEqual(list.At(i), (int) i+1);
+            }
+        }
+        
+        [TestMethod]
+        public void PushFrontTest()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            Assert.IsTrue(list.PushFront(3));
+            Assert.AreEqual(list.size, (uint) 1);
+            Assert.AreEqual(list.First(), 3);
+            Assert.AreEqual(list.Last(), 3);
+
+            Assert.IsTrue(list.PushFront(2));
+            Assert.AreEqual(list.size, (uint) 2);
+            Assert.AreEqual(list.First(), 2);
+            Assert.AreEqual(list.Last(), 3);
+
+            Assert.IsTrue(list.PushFront(1));
+            Assert.AreEqual(list.size, (uint) 3);
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 3);
+
+            for (uint i = 0; i < list.size; ++i)
+            {
+                Assert.AreEqual(list.At(i), (int) i + 1);
+            }
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            Assert.IsFalse(list.Insert(4, 3));
+            Assert.AreEqual(list.size, (uint) 0);
+            Assert.AreEqual(list.First(), 0);
+            Assert.AreEqual(list.Last(), 0);
+
+            Assert.IsTrue(list.Insert(1, 0));
+            Assert.AreEqual(list.size, (uint) 1);
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 1);
+
+            Assert.IsTrue(list.Insert(3, 1));
+            Assert.AreEqual(list.size, (uint) 2);
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 3);
+
+            Assert.IsTrue(list.Insert(2, 1));
+            Assert.AreEqual(list.size, (uint) 3);
+
+            for (uint i = 0; i < list.size; ++i)
+            {
+                Assert.AreEqual(list.At(i), (int) i + 1);
+            }
+        }
+
+        [TestMethod]
+        public void PopFrontTest()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.PushBack(1);
+            list.PushBack(2);
+            list.PushBack(3);
+
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 3);
+            Assert.AreEqual(list.size, (uint) 3);
+
+            Assert.IsTrue(list.PopFront());
+            Assert.AreEqual(list.First(), 2);
+            Assert.AreEqual(list.Last(), 3);
+            Assert.AreEqual(list.size, (uint) 2);
+
+            Assert.IsTrue(list.PopFront());
+            Assert.AreEqual(list.First(), 3);
+            Assert.AreEqual(list.Last(), 3);
+            Assert.AreEqual(list.size, (uint) 1);
+
+            Assert.IsTrue(list.PopFront());
+            Assert.AreEqual(list.First(), default);
+            Assert.AreEqual(list.Last(), default);
+            Assert.AreEqual(list.size, (uint) 0);
+
+            Assert.IsFalse(list.PopFront());
+            Assert.AreEqual(list.First(), default);
+            Assert.AreEqual(list.Last(), default);
+            Assert.AreEqual(list.size, (uint) 0);
+        }
+
+        [TestMethod]
+        public void PopBackTest()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.PushBack(1);
+            list.PushBack(2);
+            list.PushBack(3);
+
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 3);
+            Assert.AreEqual(list.size, (uint) 3);
+
+            Assert.IsTrue(list.PopBack());
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 2);
+            Assert.AreEqual(list.size, (uint) 2);
+
+            Assert.IsTrue(list.PopBack());
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 1);
+            Assert.AreEqual(list.size, (uint) 1);
+
+            Assert.IsTrue(list.PopBack());
+            Assert.AreEqual(list.First(), default);
+            Assert.AreEqual(list.Last(), default);
+            Assert.AreEqual(list.size, (uint) 0);
+
+            Assert.IsFalse(list.PopBack());
+            Assert.AreEqual(list.First(), default);
+            Assert.AreEqual(list.Last(), default);
+            Assert.AreEqual(list.size, (uint) 0);
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.PushBack(1);
+            list.PushBack(2);
+            list.PushBack(3);
+
+            Assert.IsFalse(list.Remove(3)); // invalid index
+
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 3);
+            Assert.AreEqual(list.size, (uint) 3);
+
+            Assert.IsTrue(list.Remove(1)); // remove number 2
+            Assert.AreEqual(list.At(1), 3);
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 3);
+            Assert.AreEqual(list.size, (uint) 2);
+
+            Assert.IsTrue(list.Remove(1)); // remove number 3
+            Assert.AreEqual(list.At(1), default);
+            Assert.AreEqual(list.First(), 1);
+            Assert.AreEqual(list.Last(), 1);
+            Assert.AreEqual(list.size, (uint) 1);
+
+            Assert.IsTrue(list.Remove(0));
+            Assert.AreEqual(list.At(0), default);
+            Assert.AreEqual(list.First(), default);
+            Assert.AreEqual(list.Last(), default);
+            Assert.AreEqual(list.size, (uint) 0);
+
+            Assert.IsFalse(list.Remove(0)); // remove number 1
+            Assert.AreEqual(list.At(0), default);
+            Assert.AreEqual(list.First(), default);
+            Assert.AreEqual(list.Last(), default);
+            Assert.AreEqual(list.size, (uint) 0);
+
+            
+            Assert.AreEqual(list.At(0), default); // invalid index
+            Assert.AreEqual(list.First(), default);
+            Assert.AreEqual(list.Last(), default);
+            Assert.AreEqual(list.size, (uint) 0);
+        }
+
+        [TestMethod]
+        public void EachTest()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            for (int i = 0; i < 10; ++i) list.PushBack(i);
+
+            list.Each(
+                delegate (int v, uint i)
+                {
+                    Assert.AreEqual(v, (int) i);
+                }
+            );
+            
+            list.Each(
+                delegate (int v, uint i)
+                {
+                    Assert.AreEqual(v, (int) i);
+                },
+                true
+            );
+
+        }
+    }
+}
