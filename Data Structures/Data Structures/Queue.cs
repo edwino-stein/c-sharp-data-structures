1ec8654f50af294d68719ac9d2a8317da6743fc1
Edwino Stein <edwino.stein@gmail.com>
2021-04-16 10:31:21 -0400

    Add Queue data structure

---
+++ Data Structures/Data Structures/Queue.cs
@@ -0,0 +1,66 @@
+﻿namespace DataStructures
+{
+    public class Queue<T>
+    {
+        private T[] data;
+        public uint maxSize { get; }
+        public uint size { get; private set; }
+        public uint frontPointer { get; private set; }
+        public uint backPointer { get; private set; }
+
+        public Queue(uint maxSize)
+        {
+            this.maxSize = maxSize;
+            this.size = 0;
+            this.data = new T[this.maxSize];
+            this.frontPointer = 0;
+            this.backPointer = this.maxSize - 1;
+        }
+
+        public bool Full()
+        {
+            return this.size == this.maxSize;
+        }
+
+        public bool Empty()
+        {
+            return this.size == 0;
+        }
+
+        public T Front()
+        {
+            return !this.Empty() ? this.data[this.frontPointer] : default;
+        }
+
+        public T Back()
+        {
+            return !this.Empty() ? this.data[this.backPointer] : default;
+        }
+
+        public bool Push(T item)
+        {
+            if (this.Full()) return false;
+
+            this.backPointer += 1;
+            if (this.backPointer >= this.maxSize) this.backPointer = 0;
+
+            this.data[this.backPointer] = item;
+            this.size += 1;
+
+            return true;
+        }
+
+        public bool Pop() {
+
+            if (this.Empty()) return false;
+
+            this.data[this.frontPointer] = default;
+            this.size -= 1;
+
+            this.frontPointer += 1;
+            if (this.frontPointer >= this.maxSize) this.frontPointer = 0;
+
+            return true;
+        }
+    }
+}
---
+++ Data Structures/Test/QueueTest.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures;
+
+
+namespace DataStructuresTest
+{
+    [TestClass]
+    public class QueueTest
+    {
+        [TestMethod]
+        public void Contructor()
+        {
+            Queue<int> queue = new Queue<int>(10);
+
+            Assert.AreEqual(queue.maxSize, (uint) 10);
+            Assert.AreEqual(queue.size, (uint) 0);
+
+            Assert.IsTrue(queue.Empty());
+            Assert.IsFalse(queue.Full());
+
+            Assert.AreEqual(queue.frontPointer, (uint) 0);
+            Assert.AreEqual(queue.Front(), default);
+
+            Assert.AreEqual(queue.backPointer, (uint) 9);
+            Assert.AreEqual(queue.Back(), default);
+        }
+
+        [TestMethod]
+        public void PushTest()
+        {
+            Queue<int> queue = new Queue<int>(3);
+
+            Assert.AreEqual(queue.backPointer, (uint) 2);
+
+            Assert.IsTrue(queue.Push(1));
+            Assert.AreEqual(queue.Front(), 1);
+            Assert.AreEqual(queue.Back(), 1);
+            Assert.IsFalse(queue.Full());
+            Assert.AreEqual(queue.backPointer, (uint) 0);
+
+            Assert.IsTrue(queue.Push(2));
+            Assert.AreEqual(queue.Front(), 1);
+            Assert.AreEqual(queue.Back(), 2);
+            Assert.IsFalse(queue.Full());
+            Assert.AreEqual(queue.backPointer, (uint) 1);
+
+            Assert.IsTrue(queue.Push(3));
+            Assert.AreEqual(queue.Front(), 1);
+            Assert.AreEqual(queue.Back(), 3);
+            Assert.IsTrue(queue.Full());
+            Assert.AreEqual(queue.backPointer, (uint) 2);
+
+            Assert.IsFalse(queue.Push(4));
+            Assert.AreEqual(queue.Front(), 1);
+            Assert.AreEqual(queue.Back(), 3);
+            Assert.IsTrue(queue.Full());
+            Assert.AreEqual(queue.backPointer, (uint) 2);
+        }
+
+        [TestMethod]
+        public void PopTest()
+        {
+            Queue<int> queue = new Queue<int>(3);
+
+            queue.Push(1);
+            queue.Push(2);
+            queue.Push(3);
+
+            Assert.AreEqual(queue.Front(), 1);
+            Assert.AreEqual(queue.Back(), 3);
+            Assert.AreEqual(queue.frontPointer, (uint) 0);
+            Assert.IsFalse(queue.Empty());
+
+            Assert.IsTrue(queue.Pop());
+            Assert.AreEqual(queue.Front(), 2);
+            Assert.AreEqual(queue.Back(), 3);
+            Assert.AreEqual(queue.frontPointer, (uint) 1);
+            Assert.IsFalse(queue.Empty());
+
+            Assert.IsTrue(queue.Pop());
+            Assert.AreEqual(queue.Front(), 3);
+            Assert.AreEqual(queue.Back(), 3);
+            Assert.AreEqual(queue.frontPointer, (uint) 2);
+            Assert.IsFalse(queue.Empty());
+
+            Assert.IsTrue(queue.Pop());
+            Assert.AreEqual(queue.Front(), default);
+            Assert.AreEqual(queue.Back(), default);
+            Assert.AreEqual(queue.frontPointer, (uint) 0);
+            Assert.IsTrue(queue.Empty());
+
+            Assert.IsFalse(queue.Pop());
+            Assert.AreEqual(queue.Front(), default);
+            Assert.AreEqual(queue.Back(), default);
+            Assert.AreEqual(queue.frontPointer, (uint) 0);
+            Assert.IsTrue(queue.Empty());
+        }
+
+    }
+}
