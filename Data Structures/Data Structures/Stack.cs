a367d2696d701a0806293768f03b97693d163235
Edwino Stein <edwino.stein@gmail.com>
2021-04-15 22:50:08 -0400

    Add Stack data structure

---
+++ Data Structures/Data Structures/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Stack<T>
+    {
+        private T[] data;
+        private readonly uint maxSize;
+        private uint pointer;
+
+        public Stack(uint maxSize)
+        {
+            this.maxSize = maxSize;
+            this.data = new T[maxSize + 1];
+            this.pointer = 0;
+        }
+
+        public uint Size()
+        {
+            return this.pointer;
+        }
+
+        public uint MaxSize()
+        {
+            return this.maxSize;
+        }
+
+        public bool Empty()
+        {
+            return this.Size() == 0;    
+        }
+
+        public bool Full()
+        {
+            return this.Size() >= this.MaxSize();
+        }
+
+        public bool Add(T item)
+        {
+            if (this.Full()) return false;
+            this.data[this.pointer++] = item;
+            return true;
+        }
+
+        public T Top()
+        {
+            return !this.Empty() ? this.data[this.pointer - 1] : default;
+        }
+
+        public T Pop()
+        {
+            return !this.Empty() ? this.data[(pointer--) - 1] : default;
+        }
+
+    }
+}
---
+++ Data Structures/Test/StackTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures;
+
+namespace DataStructuresTest
+{
+    [TestClass]
+    public class StackTest
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            Stack<int> stack = new Stack<int>(10);
+            Assert.AreEqual(stack.Size(), (uint) 0);
+            Assert.AreEqual(stack.MaxSize(), (uint) 10);
+            Assert.AreEqual(stack.Top(), default);
+        }
+
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Stack<int> stack = new Stack<int>(10);
+            Assert.IsTrue(stack.Empty());
+            stack.Add(123);
+            Assert.IsFalse(stack.Empty());
+        }
+
+
+        [TestMethod]
+        public void FullTest()
+        {
+            Stack<int> stack = new Stack<int>(1);
+            Assert.IsFalse(stack.Full());
+            stack.Add(123);
+            Assert.IsTrue(stack.Full());
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            Stack<int> stack = new Stack<int>(3);
+
+            Assert.IsTrue(stack.Add(1));
+            Assert.AreEqual(stack.Top(), 1);
+            Assert.IsFalse(stack.Full());
+
+            Assert.IsTrue(stack.Add(2));
+            Assert.AreEqual(stack.Top(), 2);
+            Assert.IsFalse(stack.Full());
+
+            Assert.IsTrue(stack.Add(3));
+            Assert.AreEqual(stack.Top(), 3);
+            Assert.IsTrue(stack.Full());
+
+            Assert.IsFalse(stack.Add(4));
+            Assert.AreEqual(stack.Top(), 3);
+            Assert.IsTrue(stack.Full());
+        }
+
+        [TestMethod]
+        public void PopTest()
+        {
+            Stack<int> stack = new Stack<int>(3);
+
+            Assert.AreEqual(stack.Pop(), default);
+
+            stack.Add(1);
+            stack.Add(2);
+            stack.Add(3);
+
+            Assert.AreEqual(stack.Pop(), 3);
+            Assert.IsFalse(stack.Empty());
+
+            Assert.AreEqual(stack.Pop(), 2);
+            Assert.IsFalse(stack.Empty());
+
+            Assert.AreEqual(stack.Pop(), 1);
+            Assert.IsTrue(stack.Empty());
+
+            Assert.AreEqual(stack.Pop(), default);
+        }
+    }
+}
