0ca643dc4cb0d03f6f93a95510ba132fcd859b69
Edwino Stein <edwino.stein@gmail.com>
2021-04-19 10:00:20 -0400

    Add some sort algorithms

---
+++ Data Structures/Data Structures/Sort.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures
+{
+    public class Sort <T>
+    {
+
+        public delegate bool CompareHandle(T v1, T v2);
+
+        public static bool DefaultCompareHandle(T v1, T v2)
+        {
+            // v1 >= v2
+            return Comparer<T>.Default.Compare(v1, v2) >= 0;
+        }
+
+        public static void SelectionSort(T[] data, CompareHandle compare)
+        {
+            for (int i = 0; i < data.Length - 1; ++i)
+            {
+                for (int j = i + 1; j < data.Length; ++j)
+                {
+                    if (compare(data[i], data[j]))
+                    {
+                        T temp = data[i];
+                        data[i] = data[j];
+                        data[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void SelectionSort(T[] data)
+        {
+            SelectionSort(data, DefaultCompareHandle);
+        }
+
+        public static void InsertionSort(T[] data, CompareHandle compare)
+        {
+            for (int i = 0; i < data.Length; ++i)
+            {
+                T el = data[i];
+                int j = i;
+                while (j > 0 && compare(data[j-1], el))
+                {
+                    data[j] = data[j - 1];
+                    j = j - 1;
+                }
+
+                data[j] = el;
+            }
+        }
+
+        public static void InsertionSort(T[] data)
+        {
+            InsertionSort(data, DefaultCompareHandle);
+        }
+
+        public static void BubbleSort(T[] data, CompareHandle compare)
+        {
+            bool changed = true;
+            int len = data.Length;
+
+            while (changed)
+            {
+                changed = false;
+                len -= 1;
+
+                for (int i = 0; i < len; ++i)
+                {
+                    if(compare(data[i], data[i+1]))
+                    {
+                        T temp = data[i];
+                        data[i] = data[i + 1];
+                        data[i + 1] = temp;
+                        changed = true;
+                    }
+                }
+            }
+        }
+
+        public static void BubbleSort(T[] data)
+        {
+            BubbleSort(data, DefaultCompareHandle);
+        }
+
+    }
+
+    
+}
---
+++ Data Structures/Test/SortTest.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures;
+
+namespace DataStructuresTest
+{   
+    [TestClass]
+    public class SortTest
+    {
+        private int[] data0 = new int[] { 1, 2, 3, 4, 5 };
+        private int[] data1 = new int[] { 5, 4, 3, 2, 1 };
+        private int[] data2 = new int[] { 3, 2, 5, 1, 4 };
+
+        [TestMethod]
+        public void DefaultCompareHandleTest()
+        {
+            Assert.IsFalse(Sort<int>.DefaultCompareHandle(1, 2));
+            Assert.IsTrue(Sort<int>.DefaultCompareHandle(2, 1));
+            Assert.IsTrue(Sort<int>.DefaultCompareHandle(0, 0));
+            
+            Assert.IsFalse(Sort<string>.DefaultCompareHandle("abc", "cba"));
+            Assert.IsTrue(Sort<string>.DefaultCompareHandle("cba", "abc"));
+            Assert.IsTrue(Sort<string>.DefaultCompareHandle("abc", "abc"));
+            Assert.IsTrue(Sort<string>.DefaultCompareHandle("ABC", "abc"));
+            Assert.IsFalse(Sort<string>.DefaultCompareHandle("abc", "ABC"));
+        }
+
+        [TestMethod]
+        public void SelectionSortTest()
+        {
+            int[] data = (int[]) this.data1.Clone();
+            Sort<int>.SelectionSort(data);
+            for (int i = 0; i < data.Length; ++i) Assert.AreEqual(data[i], data0[i]);
+
+            data = (int[])this.data2.Clone();
+            Sort<int>.SelectionSort(data);
+            for (int i = 0; i < data.Length; ++i) Assert.AreEqual(data[i], data0[i]);
+
+            data = (int[])this.data0.Clone();
+            Sort<int>.SelectionSort(data, (int v1, int v2) => v1 >= v2);
+            for (int i = 0; i < data.Length; ++i) Assert.AreEqual(data[i], data0[i]);
+        }
+
+        [TestMethod]
+        public void InsertionSortTest()
+        {
+            int[] data = (int[])this.data1.Clone();
+            Sort<int>.InsertionSort(data);
+            for (int i = 0; i < data.Length; ++i) Assert.AreEqual(data[i], data0[i]);
+
+            data = (int[])this.data2.Clone();
+            Sort<int>.InsertionSort(data);
+            for (int i = 0; i < data.Length; ++i) Assert.AreEqual(data[i], data0[i]);
+
+            data = (int[])this.data0.Clone();
+            Sort<int>.InsertionSort(data, (int v1, int v2) => v1 >= v2);
+            for (int i = 0; i < data.Length; ++i) Assert.AreEqual(data[i], data0[i]);
+        }
+
+        [TestMethod]
+        public void BubbleSortTest()
+        {
+            int[] data = (int[])this.data1.Clone();
+            Sort<int>.BubbleSort(data);
+            for (int i = 0; i < data.Length; ++i) Assert.AreEqual(data[i], data0[i]);
+
+            data = (int[])this.data2.Clone();
+            Sort<int>.BubbleSort(data);
+            for (int i = 0; i < data.Length; ++i) Assert.AreEqual(data[i], data0[i]);
+
+            data = (int[])this.data0.Clone();
+            Sort<int>.BubbleSort(data, (int v1, int v2) => v1 >= v2);
+            for (int i = 0; i < data.Length; ++i) Assert.AreEqual(data[i], data0[i]);
+        }
+    }
+}
