79b1b15b36bfc56d92001fc69a78b4f7d1454199
Edwino Stein <edwino.stein@gmail.com>
2021-04-19 11:30:11 -0400

    Add Binary Search Tree data structure

---
+++ Data Structures/Data Structures/BinarySearchTree.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures
+{
+    public class BinarySearchTree <T>
+    {
+        public delegate bool CompareHandle(T v1, T v2);
+        public delegate void EachHandle(T value);
+
+        public T value { get; private set; }
+        private BinarySearchTree<T>? left;
+        private BinarySearchTree<T>? right;
+        private CompareHandle compareHandle;
+
+        public BinarySearchTree(T value, CompareHandle compareHandle)
+        {
+            this.value = value;
+            this.compareHandle = compareHandle;
+            this.left = null;
+            this.right = null;
+        }
+        
+        public BinarySearchTree(T value)
+        {
+            this.value = value;
+            this.compareHandle = DefaultCompareHandle;
+            this.left = null;
+            this.right = null;
+        }
+
+        public BinarySearchTree<T> Insert(T value)
+        {
+            if(this.compareHandle(value, this.value))
+            {
+                if (this.HasRight()) return this.right.Insert(value);
+                this.right = new BinarySearchTree<T>(value, this.compareHandle);
+                return this.right;
+            }
+            else
+            {
+                if (this.HasLeft()) return this.left.Insert(value);
+                this.left = new BinarySearchTree<T>(value, this.compareHandle);
+                return this.left;
+            }
+        }
+
+        public BinarySearchTree<T>? Find(T value)
+        {
+            if (Comparer<T>.Default.Compare(this.value, value) == 0) return this;
+            if (this.compareHandle(value, this.value))
+            {
+                return this.HasRight() ? this.right.Find(value) : null;
+            }
+            else
+            {
+                return this.HasLeft() ? this.left.Find(value) : null;
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            return this.Find(value) != null;
+        }
+
+        public void EachInOrder(EachHandle handle)
+        {
+            if (this.HasLeft()) this.left.EachInOrder(handle);
+            handle(this.value);
+            if (this.HasRight()) this.right.EachInOrder(handle);
+        }
+
+        public void EachPostOrder(EachHandle handle)
+        {
+            if (this.HasLeft()) this.left.EachPostOrder(handle);
+            if (this.HasRight()) this.right.EachPostOrder(handle);
+            handle(this.value);
+        }
+
+        public void EachPreOrder(EachHandle handle)
+        {
+            handle(this.value);
+            if (this.HasLeft()) this.left.EachPreOrder(handle);
+            if (this.HasRight()) this.right.EachPreOrder(handle);
+        }
+
+        public uint Height()
+        {
+            uint leftHight = this.HasLeft() ? this.left.Height() : 0;
+            uint rightHight = this.HasRight() ? this.right.Height() : 0;
+            return (leftHight >= rightHight ? leftHight : rightHight) + 1;
+        }
+
+        public bool HasLeft()
+        {
+            return this.left != null;
+        }
+
+        public bool HasRight()
+        {
+            return this.right != null;
+        }
+
+        public bool IsLeaf()
+        {
+            return !(this.HasLeft() || this.HasRight());
+        }
+
+        public static bool DefaultCompareHandle(T v1, T v2)
+        {
+            // v1 >= v2
+            return Comparer<T>.Default.Compare(v1, v2) >= 0;
+        }
+    }
+}
---
+++ Data Structures/Test/BinarySearchTreeTest.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures;
+
+namespace DataStructuresTest
+{
+    [TestClass]
+    public class BinarySearchTreeTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            BinarySearchTree<int> root = new BinarySearchTree<int>(0);
+            Assert.AreEqual(root.value, 0);
+            Assert.IsFalse(root.HasLeft());
+            Assert.IsFalse(root.HasRight());
+            Assert.IsTrue(root.IsLeaf());
+            Assert.AreEqual(root.Height(), (uint) 1);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            BinarySearchTree<int> root = new BinarySearchTree<int>(2);
+            Assert.IsFalse(root.HasLeft());
+            Assert.IsFalse(root.HasRight());
+            Assert.AreEqual(root.Height(), (uint) 1);
+
+            var left = root.Insert(1);
+            Assert.IsTrue(root.HasLeft());
+            Assert.IsFalse(root.HasRight());
+            Assert.AreEqual(left.value, 1);
+            Assert.AreEqual(root.Height(), (uint) 2);
+
+            var right = root.Insert(3);
+            Assert.IsTrue(root.HasLeft());
+            Assert.IsTrue(root.HasRight());
+            Assert.AreEqual(right.value, 3);
+            Assert.AreEqual(root.Height(), (uint) 2);
+
+            root.Insert(0);
+            Assert.IsTrue(left.HasLeft());
+            Assert.IsFalse(left.HasRight());
+            Assert.AreEqual(root.Height(), (uint) 3);
+
+            root.Insert(4);
+            Assert.IsFalse(right.HasLeft());
+            Assert.IsTrue(right.HasRight());
+            Assert.AreEqual(root.Height(), (uint) 3);
+        }
+
+        [TestMethod]
+        public void EachTest()
+        {
+            BinarySearchTree<int> root = new BinarySearchTree<int>(3);  //        3
+            root.Insert(1);                                             //       /  \
+            root.Insert(0);                                             //     1      5
+            root.Insert(2);                                             //   /  |    |  \
+            root.Insert(5);                                             // 0    2    4    6
+            root.Insert(4);                                             //
+            root.Insert(6);                                             //
+
+            int i = 0;
+            int[] inOrder = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            root.EachInOrder(v => Assert.AreEqual(v, inOrder[i++]));
+
+            i = 0;
+            int[] postOrder = new int[] { 0, 2, 1, 4, 6, 5, 3 };
+            root.EachPostOrder(v => Assert.AreEqual(v, postOrder[i++]));
+
+            i = 0;
+            int[] preOrder = new int[] { 3, 1, 0, 2, 5, 4, 6 };
+            root.EachPreOrder(v => Assert.AreEqual(v, preOrder[i++]));
+        }
+
+        [TestMethod]
+        public void FindTest()
+        {
+            BinarySearchTree<int> root = new BinarySearchTree<int>(3);  //        3
+            root.Insert(1);                                             //       /  \
+            root.Insert(0);                                             //     1      5
+            root.Insert(2);                                             //   /  |    |  \
+            root.Insert(5);                                             // 0    2    4    6
+            root.Insert(4);                                             //
+            root.Insert(6);                                             //
+
+            Assert.IsNotNull(root.Find(3));
+            Assert.IsNotNull(root.Find(1));
+            Assert.IsNotNull(root.Find(5));
+            Assert.IsNotNull(root.Find(0));
+            Assert.IsNotNull(root.Find(6));
+            Assert.IsNull(root.Find(-1));
+            Assert.IsNull(root.Find(7));
+        }
+    }
+}
